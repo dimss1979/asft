77fbe235a29d099f4913e20fd086c95d9338a575
Dmitry Ivanov <dimss1979@yandex.ru>
2022-12-18 21:28:31 +0200

    Support more baudrates
    Also, simplify baudrate comversion from string.

--- asft_serial.c
+++ asft_serial.c
@@ -32,24 +32,41 @@ struct _asft_serial {
     } hdlc_state;
 };
 
-static struct {
-    char    *br_string;
-    speed_t  br_speed;
-} baudrate_table[] = {
-    {"300",     B300},
-    {"1200",    B1200},
-    {"115200",  B115200},
-};
-
 static speed_t string_to_baudrate(char *baudrate_string)
 {
-    for (unsigned int i = 0; i < sizeof(baudrate_table) / sizeof(baudrate_table[0]); i++) {
-        if (!strcmp(baudrate_table[i].br_string, baudrate_string)) {
-            return baudrate_table[i].br_speed;
-        }
-    }
-
-    return B0;
+    switch(atoi(baudrate_string))
+    {
+        case 50: return B50;
+        case 75: return B75;
+        case 110: return B110;
+        case 134: return B134;
+        case 150: return B150;
+        case 200: return B200;
+        case 300: return B300;
+        case 600: return B600;
+        case 1200: return B1200;
+        case 1800: return B1800;
+        case 2400: return B2400;
+        case 4800: return B4800;
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+        case 230400: return B230400;
+        case 500000: return B500000;
+        case 576000: return B576000;
+        case 921600: return B921600;
+        case 1000000: return B1000000;
+        case 1152000: return B1152000;
+        case 1500000: return B1500000;
+        case 2000000: return B2000000;
+        case 2500000: return B2500000;
+        case 3000000: return B3000000;
+        case 3500000: return B3500000;
+        case 4000000: return B4000000;
+        default: return B0;
+    };
 }
 
 static size_t hdlc_encode(unsigned char *frame_buf, unsigned char *pkt, size_t pkt_len)
